9088ace7dd427cef91271036f370ecc29aa4aa47
miguelcavada <miguel.cavada89@gmail.com>
2022-12-02 00:44:39 -0500

    Se agrega la entidad BatteryLevelsHistory. Se agrega la clase DroneController.

---
+++ Controllers/DronesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DronesAPI.Controllers
+{
+    [Route("api/[drones]")]
+    [ApiController]
+    public class DronesController : ControllerBase
+    {
+    }
+}
