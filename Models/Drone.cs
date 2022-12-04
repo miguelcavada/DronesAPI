d3662d9b3e05db84f2c34483e1cd44bd264fa619
miguelcavada <miguel.cavada89@gmail.com>
2022-12-03 19:00:26 -0500

    Se agrego las validaciones a los modelos. Se cambio el tipo de dato de campo Imagen en la clase DatabaseInitializer.

--- Models/Drone.cs
+++ Models/Drone.cs
@@ -6,16 +6,18 @@ namespace DronesAPI.Models
     {
         public Guid Id { get; set; }
 
-        [MaxLength(100)]
-        public string SerialNumber { get; set; }
+        [Required]
+        [MaxLength(100, ErrorMessage = "Maximum length for the SerialNumber is 100 characters.")]
+        public string? SerialNumber { get; set; }
 
-        public string Model { get; set; }
+        [Required]
+        public string? Model { get; set; }
 
-        [MaxLength(500)]
+        [MaxLength(500, ErrorMessage = "Maximum length for the WeightLimit is 500 g (gram).")]
         public int WeightLimit { get; set; }
 
-        public decimal BatteryCapacity { get; set; }
+        public int BatteryCapacity { get; set; }
 
-        public string State { get; set; }
+        public string? State { get; set; }
     }
 }
